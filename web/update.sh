0e096cd8dd1ee9685d54ebe1cd2ff158baddf0ff
oboard <oboard@outlook.com>
2026-02-19 21:31:01 +0800

    fix(web): improve update script robustness and cleanup
    Ensure script exits on errors and handles existing repository correctly.
    Remove old dist.zip before creating new one to avoid stale artifacts.

--- web/update.sh
+++ web/update.sh
@@ -1,10 +1,16 @@
-git clone git@github.com:oboard/msgtier-web
+#!/bin/bash
+set -e
 
-cd msgtier-web
-git pull
+if [ -d "msgtier-web" ]; then
+  cd msgtier-web
+  git pull
+else
+  git clone git@github.com:oboard/msgtier-web
+  cd msgtier-web
+fi
 
 pnpm i
-
 pnpm build
 
+rm -rf ../dist.zip
 zip -r ../dist.zip dist
